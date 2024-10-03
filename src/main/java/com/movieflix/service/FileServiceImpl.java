7811a71f0c07eb1d91ec66eabef9ac59f66df5af
harshans22 <harshhub.414@gmail.com>
2024-10-03 19:21:09 +0530

    first commit

---
+++ src/main/java/com/movieflix/service/FileServiceImpl.java
@@ -0,0 +1,42 @@
+package com.movieflix.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public String uploadFile(String path, MultipartFile file) throws IOException {
+        // get name of the file
+        String fileName = file.getOriginalFilename();
+
+        // to get the file path
+        String filePath = path + File.separator + fileName;
+
+        // create file object
+        File f = new File(path);
+        if(!f.exists()) {
+            f.mkdir();
+        }
+
+        // copy the file or upload file to the path
+        Files.copy(file.getInputStream(), Path.of(filePath));
+
+        return fileName;
+    }
+
+    @Override
+    public InputStream getResourceFile(String path, String fileName) throws FileNotFoundException {
+        String filePath = path + File.separator + fileName;
+        return new FileInputStream(filePath);
+    }
+}
